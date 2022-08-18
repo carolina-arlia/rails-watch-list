e19a5d3447bd67e072e49e72c0eb78485ed29301
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-19 01:23:15 +0200

    wip

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,13 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# un usuario puede ver todas las listas - GET lists INDEX
+# un usuario puede ver nombre y detalle de una lista especifica -GET lists/42 SHOW
+# un usuario puede crear una nueva lista - GET lists/new y POST lists NEW Y CREATE
+
+# Un usuario puede agregar un nuevo bookmark (par movie/list) en una lista existente:
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
+
+# Un usuario puede borrar un bookmark de una lista - DELETE "bookmarks/25"
