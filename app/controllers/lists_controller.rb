0e93183b904101e51d155e61547dc70fc4214c46
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-19 22:09:08 +0200

    wip

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,6 @@ class ListsController < ApplicationController
 
   def show
     @bookmark = Bookmark.new
-    @list = List.find(params[:id])
   end
 
   def new
@@ -21,6 +20,11 @@ class ListsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
