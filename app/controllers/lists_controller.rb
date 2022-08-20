3a33369b2ea3cff385eaaf585ea4dedc8c649da4
Carolina Arlia <carolinaiarlia@gmail.com>
2022-08-20 15:50:50 +0200

    image_url in model List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,7 +34,7 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:name, :photo, :image_url)
   end
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
+  validates :image_url, presence: true
 end
